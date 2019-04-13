24e944e06ebc486f6448b4600b7293e1e4310ae7
mamumusSumus <maxim>
2019-04-13 22:12:37 +0500

    An Update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        ThreadMonitor monitor = new ThreadMonitor();
-        ThreadDispatcher dispatcher = ThreadDispatcher.getInstance(monitor);
     }
 }
